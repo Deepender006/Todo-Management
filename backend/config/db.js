37c98dba8531c8874b1b5069f6aee57c6c682ffa
Deepender <rohilladeepender543@gmail.com>
2026-07-23 09:54:09 +0530

    Updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 const mongoose=require("mongoose");
 const connectDB=async()=>{
     try{
+         console.log(process.env.MONGO_URL);
         await mongoose.connect(process.env.MONGO_URL);
         console.log("MongoDB Connect");
     }catch(error){
         console.log("MongoDB Not Connected");
-        console.log(error.message);
+        console.log(error);
         process.exit(1);
     }
 };
